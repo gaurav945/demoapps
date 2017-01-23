831619487d76c9d31a7691158703f36423619611
Gaurav <oneknight2dark@gmail.com>
2017-01-24 01:19:32 +0530

    making things even with listproject

--- demoapps/urls.py
+++ demoapps/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     # Examples:
@@ -8,4 +9,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^listapp/', include('listapp.urls')),
+    url( r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
 ]
